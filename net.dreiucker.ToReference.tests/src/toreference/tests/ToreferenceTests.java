6f2ed92726de2275c448734446a66a61d89923da
Mark <mark@mopedoversum.de>
2016-04-29 17:58:22 +0200

    The ToReference plugins

---
+++ net.dreiucker.ToReference.tests/src/toreference/tests/ToreferenceTests.java
@@ -0,0 +1,46 @@
+/**
+ */
+package toreference.tests;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import junit.textui.TestRunner;
+
+/**
+ * <!-- begin-user-doc -->
+ * A test suite for the '<em><b>toreference</b></em>' package.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class ToreferenceTests extends TestSuite {
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static void main(String[] args) {
+		TestRunner.run(suite());
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static Test suite() {
+		TestSuite suite = new ToreferenceTests("toreference Tests");
+		return suite;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public ToreferenceTests(String name) {
+		super(name);
+	}
+
+} //ToreferenceTests
