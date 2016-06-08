40e390092f554d0c1ed9ab176b6ae493bd476afd
Mark <mark@mopedoversum.de>
2016-06-08 18:25:32 +0200

    Cleanup

--- net.dreiucker.DecDescLanguage.ToReference/src/net/dreiucker/decdesclanguage/toreference/ToreferenceResourceDescriptionStrategy.java
+++
@@ -1,7 +0,0 @@
-package net.dreiucker.decdesclanguage.toreference;
-
-import org.eclipse.xtext.resource.impl.DefaultResourceDescriptionStrategy;
-
-public class ToreferenceResourceDescriptionStrategy extends DefaultResourceDescriptionStrategy {
-
-}
