6d391b3731672d2b448cfae77eaae8d79b5c484a
Mark <mark@mopedoversum.de>
2016-06-09 11:49:30 +0200

    Use actual IDs ("ID" field) for requirements
    Changed the reference mechanism from the ugly UUID string 
    the more user given name

--- net.dreiucker.DecDescLanguage.ReqIf/src/net/dreiucker/decdesclanguage/reqif/QualifiedNameProvider.java
+++ net.dreiucker.DecDescLanguage.ReqIf/src/net/dreiucker/decdesclanguage/reqif/QualifiedNameProvider.java
@@ -1,7 +1,13 @@
 package net.dreiucker.decdesclanguage.reqif;
 
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.rmf.reqif10.AttributeDefinition;
+import org.eclipse.rmf.reqif10.AttributeValue;
+import org.eclipse.rmf.reqif10.AttributeValueString;
+import org.eclipse.rmf.reqif10.ReqIFContent;
 import org.eclipse.rmf.reqif10.SpecObject;
+import org.eclipse.rmf.reqif10.SpecType;
 import org.eclipse.xtext.naming.DefaultDeclarativeQualifiedNameProvider;
 import org.eclipse.xtext.naming.IQualifiedNameConverter;
 import org.eclipse.xtext.naming.QualifiedName;
@@ -10,6 +16,9 @@ import com.google.inject.Inject;
 
 public class QualifiedNameProvider  extends DefaultDeclarativeQualifiedNameProvider {
 
+	private final static String SPEC_OBJECT_NAME_ID = "ID";
+	private final static String REQUIREMENT_TYPE_LONG_NAME = "Requirement Type";
+	
 	private IQualifiedNameConverter nameConverter;
 
 	@Inject
@@ -18,23 +27,102 @@ public class QualifiedNameProvider  extends DefaultDeclarativeQualifiedNameProvi
 	}
 	
 	/**
-	 * TODO Probably need to overwrite this to map the names correctly
+	 * Find the valid fully qualified names
 	 */
 	@Override
 	public QualifiedName getFullyQualifiedName(EObject obj) {
 		QualifiedName result;
 		
 		if (obj instanceof SpecObject) {
-			SpecObject specObject = (SpecObject) obj;
-			result = nameConverter.toQualifiedName(specObject.getIdentifier());
-			
-			//TODO remove
-			System.out.println("MDD generated a new qualified name: " + result.toString());
+			result = identifySpecObjectNames((SpecObject) obj);
 		} else {
 			result = super.getFullyQualifiedName(obj); 
 		}
 		
 		return result;
+	}
+
+	/**
+	 * Identifies the user given name/ID that is associated with a spec
+	 * object.
+	 * 
+	 * @param specObject the specObject whose name is searched
+	 * @return The qualified name, or <code>null</code> if no user given name
+	 * was associated with that specObject
+	 */
+	private QualifiedName identifySpecObjectNames(SpecObject specObject) {
+		QualifiedName result = null;
+		String identifier = getIdentifier(specObject);
+		if (identifier != null) {
+			EList<AttributeValue> values = specObject.getValues();
+			for (AttributeValue value : values) { 
+				if (value instanceof AttributeValueString) {
+					AttributeValueString stringValue = (AttributeValueString) value;
+					String valueIdentifier = stringValue.getDefinition().getIdentifier();
+					if (identifier.equals(valueIdentifier)) {
+						result = nameConverter.toQualifiedName(stringValue.getTheValue());
+						break;
+					}
+				}
+			}
+		}
+		
+		//TODO remove
+		if (result != null) {
+			System.out.println("MDD generated a new qualified name: " + result.toString());
+		}
+		return result;
+	}
+
+	/**
+	 * Tries to find the identifier which is used as attribute definition that
+	 * marks the name/id of the specObject
+	 * 
+	 * @param specObject entry into the ecore hierarchy
+	 * 
+	 * @return the requested UUID or <code>null</code> in case it could not be found 
+	 */
+	private String getIdentifier(SpecObject specObject) {
+		EObject container = specObject.eContainer();
+		// search in the hierarchy for the ReqIFContent
+		while (container != null && !(container instanceof ReqIFContent)) {
+			container = container.eContainer();
+		}
+		if (container != null && (container instanceof ReqIFContent)) {
+			return findIdForNameAttribute((ReqIFContent) container);
+		}
+		return null;
+	}
+
+	/**
+	 * Find the definition of the name/id attribute and return its ID.
+	 * 
+	 * @param content
+	 * @return the requested UUID or <code>null</code> in case it could not be found 
+	 */
+	private String findIdForNameAttribute(ReqIFContent content) {
+		/*
+		 * The expected hierarchy is:
+		 * 
+		 *  ReqIFContent
+		 *  \-- SpecType (0...n) (long name = "Requirement Type")
+		 *      \-- AttributeDefinition (long name = ID")
+		 *          \-- identifier
+		 */
+		
+		EList<SpecType> specTypes = content.getSpecTypes();
+		for (SpecType type : specTypes) {
+			if (REQUIREMENT_TYPE_LONG_NAME.equals(type.getLongName())) {
+				EList<AttributeDefinition> attributes = type.getSpecAttributes();
+				for (AttributeDefinition attribute : attributes) {
+					if (SPEC_OBJECT_NAME_ID.equals(attribute.getLongName())) {
+						return attribute.getIdentifier();
+					}
+				}
+			}
+		}
+		// not found...
+		return null;
 	}
 	
 }
