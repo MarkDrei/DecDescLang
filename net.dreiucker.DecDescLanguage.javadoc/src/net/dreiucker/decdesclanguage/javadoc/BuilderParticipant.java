418e634ac49c708f9210e31797998bfb1b36e6a6
Mark <mark@mopedoversum.de>
2016-06-21 14:00:45 +0200

    Can now collect all decisions
    Now completely different approach without all this injection framework
    magic

--- net.dreiucker.DecDescLanguage.javadoc/src/net/dreiucker/decdesclanguage/javadoc/BuilderParticipant.java
+++ net.dreiucker.DecDescLanguage.javadoc/src/net/dreiucker/decdesclanguage/javadoc/BuilderParticipant.java
@@ -1,24 +1,73 @@
 package net.dreiucker.decdesclanguage.javadoc;
 
+import java.util.ArrayList;
+
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IResourceProxy;
+import org.eclipse.core.resources.IResourceProxyVisitor;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.xtext.builder.IXtextBuilderParticipant;
-import org.eclipse.xtext.resource.impl.ResourceDescriptionsProvider;
 
-import com.google.inject.Inject;
+import net.dreiucker.decDescLanguage.Decision;
+import net.dreiucker.decDescLanguage.Definition;
+import net.dreiucker.decDescLanguage.Model;
+import net.dreiucker.decDescLanguage.impl.ModelImpl;
 
 public class BuilderParticipant implements IXtextBuilderParticipant {
 
-	@Inject
-	private ResourceDescriptionsProvider resourceDescriptionsProvider;
 	
 	@Override
 	public void build(IBuildContext context, IProgressMonitor monitor) throws CoreException {
+		
+		IWorkspaceRoot myWorkspaceRoot = ResourcesPlugin.getWorkspace().getRoot();
 
-		// TODO remove
-		System.out.println("MDD BuilderParticipant.build called, ResourceDescriptionsProvider is " + resourceDescriptionsProvider);
+		ResourceCollector resourceCollector = new ResourceCollector();
+		myWorkspaceRoot.accept(resourceCollector, 0);
 		
-		
+		ResourceSet resSet = new ResourceSetImpl(); 
+		for (IResource iRes : resourceCollector.resources) {
+			String uriString = "platform:/resource" + iRes.getFullPath().toString();
+			Resource res = resSet.getResource(URI.createURI(uriString), true);
+			for (EObject content : res.getContents()) {
+				if (content instanceof Model) {
+					EList<Definition> definitions = ((ModelImpl) content).getDefinitions();
+					for (Definition def : definitions) {
+						if (def instanceof Decision) {
+							String decisionName = ((Decision) def).getName();
+							System.out.println("  MDD found a decision: " + decisionName);
+						}
+					}
+				}
+			}
+		}
 	}
 
+	private class ResourceCollector implements IResourceProxyVisitor {
+		
+		public ArrayList<IResource> resources = new ArrayList<>();
+		
+		@Override
+		public boolean visit(IResourceProxy proxy) throws CoreException {
+			if (proxy.getType() == IResource.FOLDER && proxy.getName().equals("bin")) {
+				return false;
+			}
+			if (proxy.getType() == IResource.FILE) {
+				if (proxy.getName().endsWith(".ddl")) {
+//					System.out.println("MDD: Found a DDL file: " + proxy.requestFullPath().toString());
+					resources.add(proxy.requestResource());
+				}
+				return false;
+			}
+			return true;
+		}
+	}
 }
